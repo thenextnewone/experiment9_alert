3ef1a9caa41c897e4f59cca829598df6d2d9ede1
student <student@imac-35.local>
2018-11-07 14:13:33 +0800

    Initial Commit

---
+++ alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alert
+//
+//  Created by student on 2018/11/7.
+//  Copyright © 2018年 XTX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
